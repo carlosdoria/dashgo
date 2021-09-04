c70498c74df194f50febb6a89f0b2f9d5cd22aac
carlosdoria <carlosdoria953@gmail.com>
2021-09-03 21:49:49 -0300

    feat: create sidebar responsive version

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,14 @@
-import { Flex, useBreakpointValue } from '@chakra-ui/react'
+import { Flex, useBreakpointValue, IconButton, Icon } from '@chakra-ui/react'
+import { RiMenuLine } from 'react-icons/ri'
+import { useSidebarDrawer } from '../../context/SidebarDrawerContext'
 import Logo from './Logo'
 import NotificationNax from './NotificationNax'
 import Profile from './Profile'
 import SearchBar from './SearchBar'
 
 export function Header () {
+  const { onOpen } = useSidebarDrawer()
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true
@@ -21,8 +25,20 @@ export function Header () {
       px='6'
       align='center'
     >
+      { !isWideVersion &&
+        <IconButton
+          aria-label='Open navigation'
+          icon={<Icon as={RiMenuLine} />}
+          fontSize='24'
+          variant='unstyled'
+          onClick={onOpen}
+          mr='2'
+        />
+      }
+
       <Logo />
-    { isWideVersion && <SearchBar /> }
+
+      { isWideVersion && <SearchBar /> }
     
       <Flex
         align='center'
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,23 +1,37 @@
-import { Box, Text, Stack, Link, Icon } from "@chakra-ui/react";
-import { RiContactsLine, RiDatabaseLine, RiGitMergeLine, RiInputMethodLine } from "react-icons/ri";
-import { NavLink } from "./NavLink";
-import { NavSection } from "./NavSection";
+import { Box, useBreakpointValue, Drawer, DrawerOverlay, DrawerContent, DrawerCloseButton, DrawerHeader, DrawerBody } from "@chakra-ui/react";
+import { useSidebarDrawer } from "../../context/SidebarDrawerContext";
+
+import { SidebarNav } from "./SidebarNav";
 
 export function Sidebar () {
+  const { isOpen, onClose } = useSidebarDrawer()
+
+  const isDrawerSidebar = useBreakpointValue({
+    base: true,
+    lg: false
+  })
+
+  if(isDrawerSidebar) {
+    return (
+      <Drawer isOpen={isOpen} placement='left' onClose={onClose}>
+        <DrawerOverlay>
+          <DrawerContent bg='gray.800' p='4'>
+            <DrawerCloseButton mt='6' />
+
+            <DrawerHeader>Navegação</DrawerHeader>
+
+            <DrawerBody>
+              <SidebarNav />
+            </DrawerBody>
+          </DrawerContent>
+        </DrawerOverlay>
+      </Drawer>
+    )
+  }
+
   return (
     <Box as='aside' w='64' mr='8'>
-      <Stack spacing='12' align='flex-start'>
-        <NavSection title='GERAL'>
-          <NavLink icon={RiDatabaseLine} text='Dashboard'/>
-          <NavLink icon={RiContactsLine} text='Usuários'/>
-        </NavSection>
-  
-        
-        <NavSection title='AUTOMAÇÃO'>
-          <NavLink icon={RiInputMethodLine} text='Formulários'/>
-          <NavLink icon={RiGitMergeLine} text='Automação'/>
-        </NavSection>
-      </Stack>
+      <SidebarNav />
     </Box>
   )
 }
