f471722f4a68ada24b042e529aaea8ec3f543fd8
carlosdoria <carlosdoria953@gmail.com>
2021-12-10 22:38:19 -0300

    refactor: create a hook useUsers

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -5,28 +5,10 @@ import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
-import { useQuery } from 'react-query'
-import { api } from '../../services/api'
+import { useUsers } from '../../services/hooks/useUsers';
 
 export default function UserList () {
-  const { data, isLoading, isFetching, isError } = useQuery('users', async () => {
-    const { data } = await api.get('/users')
-
-    const users = data.users.map( (user) => ({
-      id: user.id,
-      name: user.name,
-      email: user.email, 
-      createdAt: new Date(user.createdAt).toLocaleDateString('pt-br', {
-        day:'2-digit',
-        month:'long',
-        year: 'numeric'
-      }), 
-    }))
-
-    return users
-  }, {
-    staleTime: 1000 * 5 // seconds
-  })
+  const { data, isLoading, isFetching, isError } = useUsers()
 
 
   const isWideVersion = useBreakpointValue({
